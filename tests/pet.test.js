58b7064a4bd49b6657463852f6a91b1034b17735
Andy Harrison <andyhcodes@gmail.com>
2020-08-20 08:53:17 +0100

    growUp function added

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -1,7 +1,33 @@
 const Pet = require("../src/pet");
 
 describe("constructor", () => {
-  it("returns an object", () => {
+  xit("returns an object", () => {
     expect(new Pet("Fido")).toBeInstanceOf(Object);
+  });
+});
+
+describe("constructor", () => {
+  xit("has a initial age of 0", () => {
+    const pet = new Pet("Fido");
+
+    expect(pet.age).toEqual(0);
+  });
+});
+
+describe("growUp", () => {
+  xit("increments the age by 1", () => {
+    const pet = new Pet("Fido");
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+  });
+});
+
+describe("constructor", () => {
+  it("sets the name property", () => {
+    const pet = new Pet("Fido");
+
+    expect(pet.name).toEqual("Fido");
   });
 });
